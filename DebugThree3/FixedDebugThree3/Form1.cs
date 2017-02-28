4671ee2a249993f6160741435267733c376c0a82
cataclysm1987 <conklin.eric@gmail.com>
2017-02-28 14:16:47 -0500

    add files

---
+++ DebugThree3/FixedDebugThree3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FixedDebugThree3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // This declaration is in the correct place.
+        // total is outside the method so it can remain a running total.
+        double total = 0;
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+            if (textBox1 != null)
+            {
+                double num = Convert.ToDouble(textBox1.Text);
+                total = total + num;
+            }
+            outputLabel.Text = "Running total: " + total;
+        }
+    }
+}
